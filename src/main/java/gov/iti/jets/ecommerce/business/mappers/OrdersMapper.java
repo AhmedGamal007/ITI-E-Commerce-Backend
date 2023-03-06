4c68133f353b7a0880ed2c37f9cfc917d21b1d96
michaeltarekgh <michaelghaly117@gmail.com>
2023-03-07 01:07:57 +0200

    Order Mapper V2

--- src/main/java/gov/iti/jets/ecommerce/business/mappers/OrdersMapper.java
+++ src/main/java/gov/iti/jets/ecommerce/business/mappers/OrdersMapper.java
@@ -1,18 +1,19 @@
 package gov.iti.jets.ecommerce.business.mappers;
 
+
 import gov.iti.jets.ecommerce.business.dtos.OrdersDTO;
 import gov.iti.jets.ecommerce.persistence.entities.Orders;
 
 public class OrdersMapper {
 
     AddressMapper addressMapper = new AddressMapper();
-    ProductOrderMapper productOrderMapper = new ProductOrderMapper();
+    ProductOrderMapper productOrderMapper;
     public OrdersDTO maptoDto(Orders orders){
         OrdersDTO ordersDTO = new OrdersDTO();
         ordersDTO.setId(orders.getId());
         ordersDTO.setIsSubmitted(orders.getIsSubmitted());
         ordersDTO.setPaymentType(orders.getPaymentType());
-       // ordersDTO.setOrderHasProducts(productOrderMapper.orderHasProductsOrderDTO(orders, null));
+        ordersDTO.setOrderHasProducts(productOrderMapper.ordersProductToOrderProductDTO(orders.getOrderHasProducts()));
         ordersDTO.setTotalPrice(orders.getTotalPrice());
         ordersDTO.setAddressDTO(addressMapper.addresstoDTO(orders.getAddress()));
         return ordersDTO;
@@ -22,7 +23,7 @@ public class OrdersMapper {
         Orders orders = new Orders();
         orders.setId(ordersDTO.getId());
         orders.setIsSubmitted(ordersDTO.getIsSubmitted());
-      //  orders.setOrderHasProducts(pr);
+        orders.setOrderHasProducts(productOrderMapper.orderProductDTOToOrdersProduct(ordersDTO.getOrderHasProducts()));
         orders.setPaymentType(ordersDTO.getPaymentType());
         orders.setTotalPrice(ordersDTO.getTotalPrice());
         orders.setAddress(addressMapper.AdressDTOtoAddress(ordersDTO.getAddressDTO()));
--- src/main/java/gov/iti/jets/ecommerce/business/mappers/ProductOrderMapper.java
+++ src/main/java/gov/iti/jets/ecommerce/business/mappers/ProductOrderMapper.java
@@ -1,16 +1,17 @@
 package gov.iti.jets.ecommerce.business.mappers;
 
+import java.util.Set;
+
+import org.mapstruct.factory.Mappers;
+
 import gov.iti.jets.ecommerce.business.dtos.ProductsOrderDTO;
 import gov.iti.jets.ecommerce.persistence.entities.OrderHasProduct;
-import gov.iti.jets.ecommerce.persistence.entities.OrderHasProductId;
 
-public class ProductOrderMapper {
- 
-    public ProductsOrderDTO orderHasProductsOrderDTO(OrderHasProduct orderHasProduct) {
-
-        ProductsOrderDTO productsOrderDTO = new ProductsOrderDTO();
-        productsOrderDTO.setAmount(orderHasProduct.getAmount());
-        return productsOrderDTO;
-    }
+public interface ProductOrderMapper {
     
+    ProductOrderMapper mapper = Mappers.getMapper(ProductOrderMapper.class);
+
+    Set<ProductsOrderDTO> ordersProductToOrderProductDTO (Set<OrderHasProduct> product);
+   
+    Set<OrderHasProduct> orderProductDTOToOrdersProduct (Set<ProductsOrderDTO> productDTO);
 }
