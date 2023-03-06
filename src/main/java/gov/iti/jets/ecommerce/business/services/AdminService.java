ec32161583a2ea97020463744ec085b6d44930a7
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-06 17:50:58 +0200

    add find admin with userName and password

--- src/main/java/gov/iti/jets/ecommerce/business/services/AdminService.java
+++ src/main/java/gov/iti/jets/ecommerce/business/services/AdminService.java
@@ -1,5 +1,10 @@
 package gov.iti.jets.ecommerce.business.services;
 
+import gov.iti.jets.ecommerce.business.dtos.AdminDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface AdminService {
-    
+
+    public AdminDTO findAdmin(String username, String password);
 }
--- src/main/java/gov/iti/jets/ecommerce/business/servicesImpl/AdminServiceImpl.java
+++ src/main/java/gov/iti/jets/ecommerce/business/servicesImpl/AdminServiceImpl.java
@@ -1,5 +1,29 @@
 package gov.iti.jets.ecommerce.business.servicesImpl;
 
-public class AdminServiceImpl {
-    
+import gov.iti.jets.ecommerce.business.dtos.AdminDTO;
+import gov.iti.jets.ecommerce.business.mappers.AdminMapper;
+import gov.iti.jets.ecommerce.business.services.AdminService;
+import gov.iti.jets.ecommerce.persistence.entities.Admin;
+import gov.iti.jets.ecommerce.persistence.repositories.AdminRepo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminServiceImpl  implements AdminService {
+    private final AdminRepo adminRepo;
+
+    public AdminServiceImpl(AdminRepo adminRepo) {
+        this.adminRepo = adminRepo;
+    }
+
+    @Override
+    public AdminDTO findAdmin(String username, String password) {
+       Admin admin =  adminRepo.findAdminByUserNameAndPassword(username , password);
+       if(admin != null){
+           AdminDTO adminDTO = AdminMapper.INSTANCE.adminToAdminDto(admin);
+           return adminDTO;
+       }
+       return null;
+    }
+
+
 }
