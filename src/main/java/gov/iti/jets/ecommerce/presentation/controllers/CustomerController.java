1bccb62b8cebc29b6537baeebb344c5ce28efb0a
Ahmed Gamal <ahmed_gamal302@yahoo.com>
2023-03-08 01:47:12 +0200

    Basic Customer Controller Impl

--- src/main/java/gov/iti/jets/ecommerce/presentation/controllers/CustomerController.java
+++ src/main/java/gov/iti/jets/ecommerce/presentation/controllers/CustomerController.java
@@ -3,10 +3,9 @@ package gov.iti.jets.ecommerce.presentation.controllers;
 import gov.iti.jets.ecommerce.business.services.CustomerService;
 import gov.iti.jets.ecommerce.business.dtos.CustomerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/customer")
@@ -15,8 +14,27 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
     @ResponseBody
-    @GetMapping
-    public CustomerDTO getProfile(){
-    return customerService.find(2);
+    @GetMapping("/{id}")
+    public CustomerDTO getProfile(@PathVariable Integer id){
+    return customerService.find(id);
+    }
+
+    @ResponseBody
+    @GetMapping("/all")
+    public List<CustomerDTO> getAllCustomers(){
+        return customerService.getAll();
+    }
+
+   @PostMapping
+   public CustomerDTO addCustomer(@RequestBody CustomerDTO customerDTO){
+        return customerService.add(customerDTO);
+   }
+    @DeleteMapping
+    public void delete(@RequestBody CustomerDTO customerDTO){
+        customerService.delete(customerDTO);
+    }
+    @PutMapping
+    public CustomerDTO update(@RequestBody CustomerDTO customerDTO){
+        return customerService.update(customerDTO);
     }
 }
