4d26ffcbf700bcf57a0893a7126fe0fe3999e21f
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-07 22:19:27 +0200

    add admin controller

--- src/main/java/gov/iti/jets/ecommerce/presentation/controllers/AdminController.java
+++ src/main/java/gov/iti/jets/ecommerce/presentation/controllers/AdminController.java
@@ -1,12 +1,13 @@
 package gov.iti.jets.ecommerce.presentation.controllers;
 
 import gov.iti.jets.ecommerce.business.dtos.AdminDTO;
+import gov.iti.jets.ecommerce.business.dtos.AdminRegisterDTO;
 import gov.iti.jets.ecommerce.business.services.AdminService;
+
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
 
 @AllArgsConstructor
 @RestController
@@ -16,10 +17,17 @@ public class AdminController {
 
 
 
-    @GetMapping
+//    @GetMapping
+//    @ResponseBody
+//    public AdminDTO getAdmin(){
+//        return  adminService.find("mohamed","123456");
+//    }
+
+    @PostMapping
     @ResponseBody
-    public AdminDTO getAdmin(){
-        return  adminService.find("mohamed","123456");
+    public  AdminDTO add(@RequestBody AdminRegisterDTO adminRegisterDTO){
+        return  adminService.add(adminRegisterDTO);
     }
+
     
 }
