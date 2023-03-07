32a3ccd940a87a40ad14b7a385d8e61abe6b02e8
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-07 17:25:32 +0200

    add customer service interface methods

--- src/main/java/gov/iti/jets/ecommerce/business/servicesImpl/CustomerServiceImpl.java
+++ src/main/java/gov/iti/jets/ecommerce/business/servicesImpl/CustomerServiceImpl.java
@@ -1,5 +1,46 @@
 package gov.iti.jets.ecommerce.business.servicesImpl;
 
-public class CustomerServiceImpl {
-    
+import gov.iti.jets.ecommerce.business.dtos.CustomerDTO;
+import gov.iti.jets.ecommerce.business.services.CustomerService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Override
+    public List<CustomerDTO> getAll() {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO get(Integer id) {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO get(String userName, String password) {
+        return null;
+    }
+
+    @Override
+    public CustomerDTO add(CustomerDTO customerDTO) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Integer customerId) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(CustomerDTO customerDTO) {
+        return false;
+    }
+
+    @Override
+    public CustomerDTO update(CustomerDTO customerDTO) {
+        return null;
+    }
 }
