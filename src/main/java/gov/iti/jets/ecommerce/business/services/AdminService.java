f12d2032fa2774d74cad8af0ff58fdecf5bcbbb7
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-07 17:23:51 +0200

    implement admin service

--- src/main/java/gov/iti/jets/ecommerce/business/services/AdminService.java
+++ src/main/java/gov/iti/jets/ecommerce/business/services/AdminService.java
@@ -1,10 +1,15 @@
 package gov.iti.jets.ecommerce.business.services;
 
 import gov.iti.jets.ecommerce.business.dtos.AdminDTO;
+import gov.iti.jets.ecommerce.business.dtos.AdminRegisterDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface AdminService {
+    public AdminDTO find(String username, String password);
+    public AdminDTO find(Integer id);
+    public AdminDTO add(AdminRegisterDTO adminRegisterDTO);
+    public void delete(AdminDTO adminDTO);
+    public AdminDTO update(AdminDTO adminDTO);
 
-    public AdminDTO findAdmin(String username, String password);
 }
--- src/main/java/gov/iti/jets/ecommerce/business/servicesImpl/AdminServiceImpl.java
+++ src/main/java/gov/iti/jets/ecommerce/business/servicesImpl/AdminServiceImpl.java
@@ -1,28 +1,56 @@
 package gov.iti.jets.ecommerce.business.servicesImpl;
 
 import gov.iti.jets.ecommerce.business.dtos.AdminDTO;
+import gov.iti.jets.ecommerce.business.dtos.AdminRegisterDTO;
 import gov.iti.jets.ecommerce.business.mappers.AdminMapper;
 import gov.iti.jets.ecommerce.business.services.AdminService;
 import gov.iti.jets.ecommerce.persistence.entities.Admin;
 import gov.iti.jets.ecommerce.persistence.repositories.AdminRepo;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@AllArgsConstructor
 @Service
 public class AdminServiceImpl  implements AdminService {
     private final AdminRepo adminRepo;
+    private final AdminMapper adminMapper;
 
-    public AdminServiceImpl(AdminRepo adminRepo) {
-        this.adminRepo = adminRepo;
+
+    @Override
+    public AdminDTO find(String username, String password) {
+       Admin admin =  adminRepo.findAdminByUserNameAndPassword(username , password);
+       if (admin != null){
+
+       return  adminMapper.adminToAdminDto(admin);
+       }
+       return null;
     }
 
     @Override
-    public AdminDTO findAdmin(String username, String password) {
-       Admin admin =  adminRepo.findAdminByUserNameAndPassword(username , password);
-       if(admin != null){
-           AdminDTO adminDTO = AdminMapper.INSTANCE.adminToAdminDto(admin);
-           return adminDTO;
-       }
-       return null;
+    public AdminDTO find(Integer id) {
+
+        Optional <Admin> admin = adminRepo.findById(id);
+        return admin.map(adminMapper::adminToAdminDto).orElse(null);
+    }
+
+    @Override
+    public AdminDTO add(AdminRegisterDTO adminRegisterDTO) {
+       Admin admin =  adminRepo.save(adminMapper.adminRegisterDtoToAdmin(adminRegisterDTO));
+        return adminMapper.adminToAdminDto(admin);
+    }
+
+    @Override
+    public void delete(AdminDTO adminDTO) {
+        adminRepo.delete(adminMapper.adminDtoToAdmin(adminDTO));
+    }
+
+    @Override
+    public AdminDTO update(AdminDTO adminDTO) {
+        Admin admin = adminRepo.save(adminMapper.adminDtoToAdmin(adminDTO));
+        return  adminMapper.adminToAdminDto(admin);
     }
 
 
