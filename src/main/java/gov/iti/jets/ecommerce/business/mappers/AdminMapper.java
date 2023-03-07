3c414d208833124baf621b5e13ab0c6949f0d38e
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-07 17:20:21 +0200

    edit admin and customer mapper

--- src/main/java/gov/iti/jets/ecommerce/business/mappers/AdminMapper.java
+++ src/main/java/gov/iti/jets/ecommerce/business/mappers/AdminMapper.java
@@ -1,16 +1,16 @@
 package gov.iti.jets.ecommerce.business.mappers;
 
 import gov.iti.jets.ecommerce.business.dtos.AdminDTO;
+import gov.iti.jets.ecommerce.business.dtos.AdminRegisterDTO;
 import gov.iti.jets.ecommerce.persistence.entities.Admin;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface AdminMapper {
-    AdminMapper INSTANCE = Mappers.getMapper(AdminMapper.class);
-
 
     AdminDTO adminToAdminDto(Admin admin);
 
     Admin adminDtoToAdmin(AdminDTO adminDTO);
+
+    Admin adminRegisterDtoToAdmin(AdminRegisterDTO adminRegisterDTO);
 }
