bd62d4fc8f3be27ac2d85311b253e6f572c390af
AhmedHesham997 <ahmedshafikbeder99@gmail.com>
2023-03-08 10:13:18 +0200

    solving conflicts

--- src/main/java/gov/iti/jets/ecommerce/presentation/controllers/ProductsController.java
+++ src/main/java/gov/iti/jets/ecommerce/presentation/controllers/ProductsController.java
@@ -2,8 +2,6 @@ package gov.iti.jets.ecommerce.presentation.controllers;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import com.zaxxer.hikari.pool.ProxyDatabaseMetaData;
 import gov.iti.jets.ecommerce.business.dtos.ProductCategoriesDTO;
 import gov.iti.jets.ecommerce.business.services.ProductService;
 
