b936c545733329269b8664038364c733dfc6aa39
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-11 01:42:37 +0200

    Request and Response for auth and register

--- src/main/java/gov/iti/jets/ecommerce/presentation/controllers/AuthController.java
+++ src/main/java/gov/iti/jets/ecommerce/presentation/controllers/AuthController.java
@@ -1,7 +1,9 @@
 package gov.iti.jets.ecommerce.presentation.controllers;
 
+import gov.iti.jets.ecommerce.business.dtos.AuthRequest;
+import gov.iti.jets.ecommerce.business.dtos.AuthResponse;
+import gov.iti.jets.ecommerce.business.dtos.RegisterRequest;
 import gov.iti.jets.ecommerce.business.services.AuthService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,11 +15,11 @@ public class AuthController {
 
     private final AuthService service ;
     @PostMapping("/register")
-    public ResponseEntity<AuthResponse> register(@RequestBody  RegisterRequest request){
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
         return ResponseEntity.ok(service.register(request));
     }
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthResponse> register(@RequestBody  AuthRequest request){
+    public ResponseEntity<AuthResponse> register(@RequestBody AuthRequest request){
         return ResponseEntity.ok(service.authenticate(request));
     }
 
