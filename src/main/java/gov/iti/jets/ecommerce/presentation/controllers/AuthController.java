d128c77d7b6c257c2c7b3c9e9eadbf4ac87c2043
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-11 01:29:52 +0200

    security and Jwt configuration

---
+++ src/main/java/gov/iti/jets/ecommerce/presentation/controllers/AuthController.java
@@ -0,0 +1,25 @@
+package gov.iti.jets.ecommerce.presentation.controllers;
+
+import gov.iti.jets.ecommerce.business.services.AuthService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService service ;
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody  RegisterRequest request){
+        return ResponseEntity.ok(service.register(request));
+    }
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthResponse> register(@RequestBody  AuthRequest request){
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+}
+
