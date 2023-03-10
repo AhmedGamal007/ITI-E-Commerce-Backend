621b991392ec37d815807feb091a52400eb4d9c7
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-11 01:31:27 +0200

    Auth and Jwt Service

---
+++ src/main/java/gov/iti/jets/ecommerce/business/services/AuthService.java
@@ -0,0 +1,50 @@
+package gov.iti.jets.ecommerce.business.services;
+
+import gov.iti.jets.ecommerce.persistence.entities.Customer;
+import gov.iti.jets.ecommerce.persistence.entities.Role;
+import gov.iti.jets.ecommerce.persistence.repositories.CustomerRepo;
+import gov.iti.jets.ecommerce.presentation.controllers.AuthRequest;
+import gov.iti.jets.ecommerce.presentation.controllers.AuthResponse;
+import gov.iti.jets.ecommerce.presentation.controllers.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final CustomerRepo userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private  final  JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    public AuthResponse register(RegisterRequest request) {
+        var user = new Customer();
+        user.setUsername(request.getUserName());
+        user.setEmail(request.getEmail());
+        user.setPhone(request.getPhone());
+        user.setRole(Role.CUSTOMER);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        userRepo.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthResponse authenticate(AuthRequest request) {
+         authenticationManager.authenticate(
+                 new UsernamePasswordAuthenticationToken(
+                         request.getUserName(),
+                         request.getPassword()
+                 )
+         );
+         var user = userRepo.findCustomerByUsername(request.getUserName()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
