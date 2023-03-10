8c638059c2061c37313200eed5e33571652977b1
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-11 01:15:02 +0200

    extend UserDetails , add Role  and change annotation to be field-based annotation.

--- src/main/java/gov/iti/jets/ecommerce/persistence/entities/User.java
+++ src/main/java/gov/iti/jets/ecommerce/persistence/entities/User.java
@@ -1,89 +1,75 @@
 package gov.iti.jets.ecommerce.persistence.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import static jakarta.persistence.GenerationType.IDENTITY;
-import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "user", catalog = "ecommerce", uniqueConstraints = { @UniqueConstraint(columnNames = "email"),
         @UniqueConstraint(columnNames = "user_name") })
 @Inheritance(strategy = InheritanceType.JOINED)
-public abstract class User implements java.io.Serializable {
-
-    private Integer id;
-    private String userName;
-    private String email;
-    private String password;
-    private String phone;
-
-    public User() {
-    }
-
-    public User(String userName, String email, String password) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-    }
-
-    public User(String userName, String email, String password, String phone) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-        this.phone = phone;
-    }
+public abstract class User implements java.io.Serializable , UserDetails {
 
     @Id
     @GeneratedValue(strategy = IDENTITY)
 
     @Column(name = "id", unique = true, nullable = false)
-    public Integer getId() {
-        return this.id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
+    private Integer id;
     @Column(name = "user_name", unique = true, nullable = false, length = 45)
-    public String getUserName() {
-        return this.userName;
-    }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
+    private String username;
     @Column(name = "email", unique = true, nullable = false, length = 150)
-    public String getEmail() {
-        return this.email;
-    }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String email;
+    @Column(name = "password", nullable = false, length = 255)
 
-    @Column(name = "password", nullable = false, length = 150)
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
+    private String password;
     @Column(name = "phone", length = 20)
-    public String getPhone() {
-        return this.phone;
+
+    private String phone;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
-    public void setPhone(String phone) {
-        this.phone = phone;
+    @Override
+    public String getUsername() {
+        return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
