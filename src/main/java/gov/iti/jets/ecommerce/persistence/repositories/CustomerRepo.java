eb2a80ccf39b420460000a8ea9e6e1ea0dd95105
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-11 01:32:49 +0200

    Add find by username

--- src/main/java/gov/iti/jets/ecommerce/persistence/repositories/CustomerRepo.java
+++ src/main/java/gov/iti/jets/ecommerce/persistence/repositories/CustomerRepo.java
@@ -3,8 +3,12 @@ package gov.iti.jets.ecommerce.persistence.repositories;
 import gov.iti.jets.ecommerce.persistence.entities.Admin;
 import gov.iti.jets.ecommerce.persistence.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface CustomerRepo extends JpaRepository<Customer,Integer> {
-    Customer findCustomerByUserNameAndPassword(String userName , String password);
+    Customer findCustomerByUsernameAndPassword(String userName , String password);
 
+    Optional<Customer> findCustomerByUsername(String userName);
 }
