47e0cb8df104c4d7f69ad8f7f67a3634b9f4921c
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-03-15 14:47:59 +0200

    add Address Controller

--- src/main/java/gov/iti/jets/ecommerce/persistence/repositories/AddressRepo.java
+++ src/main/java/gov/iti/jets/ecommerce/persistence/repositories/AddressRepo.java
@@ -1,2 +1,10 @@
-package gov.iti.jets.ecommerce.persistence.repositories;public interface AddressRepo {
+package gov.iti.jets.ecommerce.persistence.repositories;
+
+import gov.iti.jets.ecommerce.persistence.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Set;
+
+public interface AddressRepo extends JpaRepository<Address,Integer> {
+    Set<Address> findAllByCustomerId(Integer id);
 }
